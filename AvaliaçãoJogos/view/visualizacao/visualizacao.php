a49deefecdba20378fad4377c282a37878457a8c
Fernando.Dadalto <fgdadalto@gmail.com>
2024-11-12 08:43:16 -0300

    Add files via upload

---
+++ AvaliaçãoJogos/view/visualizacao/visualizacao.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detalhes do Jogo</title>
+    <link rel="stylesheet" href="visualizacao.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <div class="logo">
+                <img src="imagemEmpresa.jpg" alt="Logo">
+            </div>
+            <nav>
+                <a href="#">Início</a>
+                <a href="#">Ranking</a>
+                <a href="#">Cadastrar Jogos</a>
+            </nav>
+            <div class="barra-pesquisa">
+            <input type="text" placeholder="Pesquisar...">
+            <button>🔍</button>
+            <button>🔒</button>
+            </div>
+        </header>
+
+        <div class="content">
+            <img src="God_of_War_II_.png" alt="God of War">
+            <div class="info">
+                <p><strong>Nome:</strong> God of War II</p>
+                <p><strong>Lançamento:</strong> 13/03/2007</p>
+                <p><strong>Plataformas:</strong> PlayStation 2, PlayStation 3, PlayStation Vita</p>
+                <p><strong>Gênero:</strong> Ação e Aventura</p>
+                <p><strong>Modos de Jogo:</strong> Um jogador</p>
+                <p><strong>Características:</strong> Hack and Slash, Quick Time Events</p>
+                <p><strong>Temas:</strong> Mitologia Grega</p>
+                <p><strong>Desenvolvedora:</strong> SCE Santa Monica Studio</p>
+                <p><strong>Publicadora:</strong> Sony Computer Entertainment</p>
+                <p><strong>Franquia:</strong> God of War</p>
+            </div>
+            <!-- Sinopse -->
+            <h2>Sinopse</h2>
+            <p class="sinopse">
+                Ao derrotar Ares, Kratos, o antigo guerreiro mortal, torna-se o novo Deus da Guerra. Porém, Kratos logo se vê sozinho no Olimpo, desprezado por seus companheiros deuses.
+            </p>
+            <p class="sinopse">
+                God of War II é um jogo de ação e aventura hack-and-slash e a sequência de God of War. Lançado inicialmente em março de 2007 na América do Norte e depois na Europa e na Austrália, o jogo recebeu aclamação da crítica por sua jogabilidade, gráficos e história envolvente. Kratos, o personagem principal, continua sua jornada vingativa, enfrentando novos desafios e criaturas da mitologia grega.
+            </p>
+            <p class="sinopse">
+                O enredo se aprofunda na busca de Kratos por poder e vingança contra os deuses do Olimpo, levando-o a uma série de eventos épicos e cenas cinematográficas impressionantes que definiram um novo padrão para o gênero. A jogabilidade oferece uma mistura de combates intensos, resolução de enigmas e eventos de "quick time", garantindo uma experiência envolvente para os jogadores.
+            </p><br><br>
+
+            <h2>Comentários</h2>
+            <form class="comment-section">
+                <textarea placeholder="Deixe um comentário..."></textarea>
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
